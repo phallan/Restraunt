f8f8e5469aefff1852ec30cf73a75998841b9573
Laash <Laash@DESKTOP-PPNPE7P>
2016-12-18 21:51:15 -0500

    css

--- Restraunt/Controllers/HomeController.cs
+++ Restraunt/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Restraunt.Controllers
         public ActionResult About()
         {
             ViewBag.Menu = "  We plan to bring sizzling spices of Karahi in the forms of . Dhaba karahi is unique for its roadside dhaba flavors that are made by villagers in India. Chikkar cholay (Channa or Chickpea Masala) is a heartwarming recipe passed down from cholay experts in Punjab. l translate the unmatched flavors brought all the way from Lahore.";
-            ViewBag.inspriration = "why we are making this restraunt";
+            ViewBag.inspriration = "We are running this restraunt to provide the tradiditional and authentictic Indian taste to our customers";
 
 
             return View();
@@ -24,7 +24,7 @@ namespace Restraunt.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "You can contact us here:-";
+            ViewBag.Message = "Please let us know about our services.You can contact us here:-";
             
 
             return View();
